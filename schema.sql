444fd206bb376ffa7387a11d08dd442792439f47
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-01-18 18:39:40 -0800

    updated data structure

--- schema.sql
+++ schema.sql
@@ -12,15 +12,32 @@ CREATE TABLE projects
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE time
+CREATE TABLE instance
 (
 	id int NOT NULL AUTO_INCREMENT,
+	user_id INT NOT NULL,
 	project_id int NOT NULL,
-    timeIN timestamp default current_timestamp NOT NULL,
-    timeOut timestamp default current_timestamp NOT NULL,
+    timeIN TIMESTAMP NOT NULL,
+    timeOut TIMESTAMP NOT NULL,
 	PRIMARY KEY (id),
-    FOREIGN KEY (project_id) REFERENCES projects(id)
-   
-    
+    FOREIGN KEY (project_id) REFERENCES projects(id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
+
 );
 
+CREATE TABLE user
+(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR (30) NOT NULL,
+	last_name VARCHAR (30) NOT NULL
+)
+
+USE sundial;
+INSERT INTO projects(project_name, project__billing_id)
+VALUES ('lost','l01'),('found','f01'),('uncertain','u01');
+
+INSERT INTO instance(project_id, timeIN,timeOut)
+VALUES(1,1,'2021-01-17 15:03:00','2021-01-17 15:30:00')
+;
+INSERT INTO user(first_name, last_name, instance_id)
+VALUES ('Aaron', 'Parnell')
