0822442e897e5905ff5a507c5f793134a3c2f89b
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-01-21 01:16:26 -0800

    fixed api route for index page to render actual data

--- db/schema.sql
+++ db/schema.sql
@@ -4,40 +4,12 @@ DROP DATABASE IF EXISTS sundial;
 CREATE DATABASE sundial;
 
 USE sundial;
-CREATE TABLE projects
-(
-	id int NOT NULL AUTO_INCREMENT,
-	project_name varchar(255) NOT NULL,
-	project__billing_id varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE instance
-(
-	id int NOT NULL AUTO_INCREMENT,
-	user_id INT NOT NULL,
-	project_id int NOT NULL,
-    timeIN TIMESTAMP NOT NULL,
-    timeOut TIMESTAMP NOT NULL,
-	PRIMARY KEY (id),
-    FOREIGN KEY (project_id) REFERENCES projects(id),
-    FOREIGN KEY (user_id) REFERENCES user(id)
-
-);
-
-CREATE TABLE user
-(
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	first_name VARCHAR (30) NOT NULL,
-	last_name VARCHAR (30) NOT NULL
-)
-
-USE sundial;
-INSERT INTO projects(project_name, project__billing_id)
+INSERT INTO projects(projectName, projectNumber)
 VALUES ('lost','l01'),('found','f01'),('uncertain','u01');
 
-INSERT INTO instance(project_id, timeIN,timeOut)
+INSERT INTO users(firstName, lastName)
+VALUES ('Aaron', 'Parnell');
+
+INSERT INTO instances(projectId,userId, timeIN,timeOut)
 VALUES(1,1,'2021-01-17 15:03:00','2021-01-17 15:30:00')
 ;
-INSERT INTO user(first_name, last_name, instance_id)
-VALUES ('Aaron', 'Parnell')
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,12 +5,17 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  // Here we add an "include" property to our options in our findAll query
-  // We set the value to an array of the models we want to include in a left outer join
-  // In this case, just db.Post
-  // db.Users.findAll({}).then(( data ) => {
-  //   console.log(data);
-  res.render("index");
+  db.User.findAll({}).then(users => {
+    const usersObj = {
+      names: users.map(data => {
+        return {
+          firstName: data.firstName,
+          lastName: data.lastName
+        };
+      })
+    };
+    res.render("index", { users: usersObj.names });
+  });
 });
 
 module.exports = router;
