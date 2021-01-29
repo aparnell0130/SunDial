940696b4790fc93c28da7cfa4e77179d05902020
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-01-29 08:21:59 -0800

    updated get 1 user api routes generated random color for chart

--- public/assets/js/shift.js
+++ public/assets/js/shift.js
@@ -179,6 +179,15 @@ endShiftButtonEl.on("click", event => {
   function chartIt(xLabels, yData) {
     console.log(xLabels);
     console.log(yData);
+    const colors = [];
+    for (let i = 0; i < xLabels.length; i++) {
+      const o = Math.round,
+        r = Math.random,
+        s = 255;
+      const color =
+        "rgb(" + o(r() * s) + "," + o(r() * s) + "," + o(r() * s) + ")";
+      colors.push(color);
+    }
     const ctx = document.getElementById("myChart").getContext("2d");
     const myChart = new Chart(ctx, {
       type: "doughnut",
@@ -188,7 +197,7 @@ endShiftButtonEl.on("click", event => {
           {
             label: "Shift Time",
             data: yData,
-            backgroundColor: [],
+            backgroundColor: colors,
             borderWidth: 1
           }
         ]
@@ -197,38 +206,11 @@ endShiftButtonEl.on("click", event => {
         scales: {
           yAxes: [
             {
-              label: "Shift Time",
-              data: yData,
-              backgroundColor: [
-                // "rgba(255, 99, 132, 0.2)",
-                // "rgba(54, 162, 235, 0.2)",
-                // "rgba(255, 206, 86, 0.2)",
-                // "rgba(75, 192, 192, 0.2)",
-                // "rgba(153, 102, 255, 0.2)",
-                // "rgba(255, 159, 64, 0.2)"
-              ],
-              borderColor: [
-                // "rgba(255, 99, 132, 1)",
-                // "rgba(54, 162, 235, 1)",
-                // "rgba(255, 206, 86, 1)",
-                // "rgba(75, 192, 192, 1)",
-                // "rgba(153, 102, 255, 1)",
-                // "rgba(255, 159, 64, 1)"
-              ],
-              borderWidth: 1
+              ticks: {
+                beginAtZero: true
+              }
             }
           ]
-        },
-        options: {
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true
-                }
-              }
-            ]
-          }
         }
       }
     });
