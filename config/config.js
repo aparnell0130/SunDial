df947f5f2d5a4f8b6a918babc3abe94af1417ce3
Danny Maier <dannymaier@gmail.com>
2021-01-29 17:26:51 -0800

    committing to be able to pull down updates

--- config/config.js
+++ config/config.js
@@ -2,8 +2,10 @@ require("dotenv").config(); // this is important!
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "password",
+    // username: process.env.DB_USER || "root",
+    // password: process.env.DB_PASS || "password",
+    username: "root",
+    password: "6Japan9!",
     database: "sundial",
     host: "127.0.0.1",
     dialect: "mysql"
