9e27ae959a310131679207548ac6b828062adc9a
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-02-12 17:34:08 -0800

    fixed projects page

--- public/assets/js/projects.js
+++ public/assets/js/projects.js
@@ -1,32 +1,27 @@
 const instanceProjects = $("#instanceDataProjects");
-const userH1 = $("#userH1");
-userH1.load(function(event) {
-  event.preventDefault();
-  instanceProjects.empty();
-  // userH1.empty();
-  const userId = $(this).data("id");
-  const deltaT = data => {
-    for (let i = 0; i < data.length; i++) {
-      const instanceElement = data[i];
-      const timeIn = instanceElement.timeIn;
-      const timeOut = instanceElement.timeOut;
-      const time1 = moment(timeIn.split(" ").join("T"));
-      const time2 = moment(timeOut.split(" ").join("T"));
-      const timeSpent = time2.diff(time1, "hours", true);
-      instanceElement.timeSpent = timeSpent;
-    }
-  };
-  $.get("/api/chartingInstances/all/" + userId).then(data => {
-    deltaT(data);
-    for (let i = 0; i < data.length; i++) {
-      instanceProjects.append(
-        `<tr id="${data[i].id}">
+
+const userId = $("#userH1").data("id");
+const deltaT = data => {
+  for (let i = 0; i < data.length; i++) {
+    const instanceElement = data[i];
+    const timeIn = instanceElement.timeIn;
+    const timeOut = instanceElement.timeOut;
+    const time1 = moment(timeIn.split(" ").join("T"));
+    const time2 = moment(timeOut.split(" ").join("T"));
+    const timeSpent = time2.diff(time1, "hours", true);
+    instanceElement.timeSpent = timeSpent;
+  }
+};
+$.get("/api/chartingInstances/all/" + userId).then(data => {
+  deltaT(data);
+  for (let i = 0; i < data.length; i++) {
+    instanceProjects.append(
+      `<tr id="${data[i].id}">
         <td>${data[i].projectName}</td>
         <td>${data[i].timeIn}</td>
         <td>${data[i].timeOut}</td>
         <td>${data[i].timeSpent.toFixed(2)}</td>
         </tr>`
-      );
-    }
-  });
+    );
+  }
 });
